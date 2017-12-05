18d1420c20839e7e52bdf8aae5155383cb8e9457
Martin Bagge <martin.bagge@glesys.se>
2017-12-05 08:18:37 +0100

    Extend 1a with test path.

--- 2017/1/day1part1.sh
+++ 2017/1/day1part1.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 
-data=1122     # 3
-data=1111     # 4
-data=1234     # 0
-data=91212129 # 9
+testdata=(1122 1111 1234 91212129)
+testexpected=(3 4 0 9)
 
-data=$1
+if [[ $1 ]]; then
+	data=$1
+fi
 
-# Split string per char to array.
-mapfile -t splitted < <(for ((i=0;i<${#data};i++)); do echo "${data:$i:1}"; done)
+checkSolution () {
+	local string=$1
+	local inputdata
+	# Split string per char to array.
+	mapfile -t inputdata < <(for ((i=0;i<${#string};i++)); do echo "${string:$i:1}"; done)
+	result=0
+	for ((i=0;i<${#inputdata[@]};i++)); do
+		this=${inputdata[$i]}
+		next=${inputdata[$((i+1))]}
+		# Wrap around for the last element.
+		if (( $((i+1)) == ${#inputdata[@]} )); then
+			next=${inputdata[0]}
+		fi
+		if [[ $this == "$next" ]]; then
+			result=$((result+this))
+		fi
+	done
+	echo "$result"
+}
 
-result=0
-for ((i=0;i<${#splitted[@]};i++)); do
-	this=${splitted[$i]}
-	next=${splitted[$((i+1))]}
-	# Wrap around for the last element.
-	if (( $((i+1)) == ${#splitted[@]} )); then
-		next=${splitted[0]}
+assertEqual () {
+	local expected=$1
+	local actual=$2
+	local msg=$3
+	if [[ $actual != "$expected" ]]; then
+		echo "Unable to assert that $actual is equal to $expected. ${msg:-}"
+		exit 1
 	fi
-	if [[ $this == "$next" ]]; then
-		result=$((result+this))
-	fi
+}
+
+echo -n "Execute tests..."
+for ((i=0;i<${#testdata[@]};i++)); do
+	assertEqual "${testexpected[$i]}" "$(checkSolution ${testdata[$i]})"
 done
-echo "Sum: $result"
+echo "OK"
+if [[ $data ]]; then
+	echo "Finding solution..."
+	checkSolution "$data"
+fi
