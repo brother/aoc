27c13589b27f6de8c11433e7eb494fbf54a3467e
Martin Bagge <martin.bagge@glesys.se>
2017-12-05 08:26:19 +0100

    Extend 1b with test path.

--- 2017/1/day1part2.sh
+++ 2017/1/day1part2.sh
@@ -1,30 +1,53 @@
 #!/bin/bash
 
-data=1212     # 6
-data=1221     # 0
-data=123425   # 4
-data=123123   # 12
-data=12131415 # 4
+testdata=(1212 1221 123425 123123 12131415)
+testexpected=(6 0 4 12 4)
 
-data=$1
+if [[ $1 ]]; then
+	data=$1
+fi
 
-# Split string per char to array.
-mapfile -t splitted < <(for ((i=0;i<${#data};i++)); do echo "${data:$i:1}"; done)
+checkSolution () {
+	local string=$1
+	local inputdata
+	# Split string per char to array.
+	mapfile -t inputdata < <(for ((i=0;i<${#string};i++)); do echo "${string:$i:1}"; done)
 
-result=0
-nbrofelements=${#splitted[@]}
-lookahead=$((nbrofelements/2))
-for ((i=0;i<nbrofelements;i++)); do
-	this=${splitted[$i]}
-	nextindex=$((i+lookahead))
-	next=${splitted[$nextindex]}
-	# Reconsider when outside of the length
-	if (( nextindex >= nbrofelements )); then
-		left=$((nextindex-nbrofelements))
-		next=${splitted[$((0+left))]}
+	result=0
+	nbrofelements=${#inputdata[@]}
+	lookahead=$((nbrofelements/2))
+	for ((i=0;i<nbrofelements;i++)); do
+		this=${inputdata[$i]}
+		nextindex=$((i+lookahead))
+		next=${inputdata[$nextindex]}
+		# Reconsider when outside of the length
+		if (( nextindex >= nbrofelements )); then
+			left=$((nextindex-nbrofelements))
+			next=${inputdata[$((0+left))]}
+		fi
+		if [[ $this == "$next" ]]; then
+			result=$((result+this))
+		fi
+	done
+	echo "$result"
+}
+
+assertEqual () {
+	local expected=$1
+	local actual=$2
+	local msg=$3
+	if [[ $actual != "$expected" ]]; then
+		echo "Unable to assert that $actual is equal to $expected. ${msg:-}"
+		exit 1
 	fi
-	if [[ $this == "$next" ]]; then
-		result=$((result+this))
-	fi
+}
+
+echo -n "Execute tests..."
+for ((i=0;i<${#testdata[@]};i++)); do
+	assertEqual "${testexpected[$i]}" "$(checkSolution ${testdata[$i]})"
 done
-echo "$result"
+echo "OK"
+if [[ $data ]]; then
+	echo "Finding solution..."
+	checkSolution "$data"
+fi
